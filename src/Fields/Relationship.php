3d044bb305e98a0784afafd37b73962e22a85d7d
andré felipe <andre@andrefelipe.com>
2020-09-28 10:17:26 -0300

    added relationship fields methods

--- src/Fields/Relationship.php
+++ src/Fields/Relationship.php
@@ -3,9 +3,81 @@
 namespace Bond\Fields;
 
 /**
- * @method self returnFormat(string $type)
+ *
  */
 class Relationship extends Field
 {
     protected string $type = 'relationship';
+    public string $return_format = 'id';
+    public array $filters = [
+        'search',
+        'post_type',
+        'taxonomy',
+    ];
+
+    public function postType(array $post_types): self
+    {
+        $this->post_type = $post_types;
+        return $this;
+    }
+
+    public function taxonomy(array $taxonomies): self
+    {
+        $this->taxonomy = $taxonomies;
+        return $this;
+    }
+
+    public function filters(array $filters): self
+    {
+        $this->filters = $filters;
+        return $this;
+    }
+
+    public function removeFilters(): self
+    {
+        $this->filters = [];
+        return $this;
+    }
+
+    public function removeSearchFilter(): self
+    {
+        $this->filters = array_diff($this->filters, ['search']);
+        return $this;
+    }
+
+    public function removePostTypeFilter(): self
+    {
+        $this->filters = array_diff($this->filters, ['post_type']);
+        return $this;
+    }
+
+    public function removeTaxonomyFilter(): self
+    {
+        $this->filters = array_diff($this->filters, ['taxonomy']);
+        return $this;
+    }
+
+    public function elements(array $elements): self
+    {
+        $this->elements = $elements;
+        return $this;
+    }
+
+    public function min(int $min): self
+    {
+        $this->min = $min;
+        return $this;
+    }
+
+    public function max(int $max): self
+    {
+        $this->max = $max;
+        return $this;
+    }
+
+    public function returnFormat(string $type): self
+    {
+        $this->return_format = $type;
+        return $this;
+    }
 }
