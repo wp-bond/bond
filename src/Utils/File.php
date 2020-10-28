2b5a2792c0778ab7f9912ec2c61574fe485bb30e
andré felipe <andre@andrefelipe.com>
2020-10-28 09:40:43 -0300

    merge file and attachment utils

--- src/Utils/File.php
+++ src/Utils/File.php
@@ -9,6 +9,49 @@ namespace Bond\Utils;
  */
 class File
 {
+    public static function url($file_id): string
+    {
+        $id = Cast::postId($file_id);
+        return $id
+            ? (string) \wp_get_attachment_url($id)
+            : (string) $file_id;
+    }
+
+    public static function size($file_id): int
+    {
+        $local_file = \get_attached_file($file_id, true);
+
+        if (empty($local_file) || !file_exists($local_file)) {
+            return 0;
+        }
+        return filesize($local_file);
+    }
+
+    public static function sizeFormat(
+        $file_id,
+        string $format = 'mb',
+        bool $with_unit = true
+    ): string {
+
+        $size = self::size($file_id);
+        $unit = '';
+
+        switch (strtolower($format)) {
+            case 'mb':
+            case 'mega':
+            case 'megabytes':
+                $size = number_format($size / 1048576, 2);
+                $unit = 'MB';
+                break;
+
+            case 'bytes':
+            default:
+                $unit = 'bytes';
+                break;
+        }
+
+        return $size . ($with_unit ? ' ' . $unit : '');
+    }
 
     public static function get(string $path): string
     {
