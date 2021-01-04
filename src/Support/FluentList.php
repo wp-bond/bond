457205bfe38baacbc0f51956910bf38bbc1d62d3
andré felipe <andre@andrefelipe.com>
2021-01-04 13:10:14 -0300

    fix object serialization on FluentList

--- src/Support/FluentList.php
+++ src/Support/FluentList.php
@@ -139,7 +139,7 @@ class FluentList implements
 
     public function __serialize(): array
     {
-        return $this->toArray();
+        return $this->all();
     }
 
     public function __unserialize(array $items)
