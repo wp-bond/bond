ca7220367b17def96fe82a3c0cece57d972a3c68
andré felipe <andre@addd.studio>
2022-12-22 10:58:21 -0300

    http helper to get a remove file size

---
+++ src/Utils/Http.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bond\Utils;
+
+/**
+ * Provides some helpers for remote files.
+ */
+class Http
+{
+    public static function contentLength($url): ?int
+    {
+        return cache()->remember(
+            'bond/http/content-length-' . md5($url),
+            function () use ($url) {
+
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HEADER, true);
+                curl_setopt($ch, CURLOPT_NOBODY, true);
+                curl_exec($ch);
+                $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+                curl_close($ch);
+
+                return $size ?: null;
+            },
+            -1
+        );
+    }
+}
